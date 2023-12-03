4faeb34d78a4ba088ff7eee8cb9a3c3a8f509aed
Robin <mohammadrobin636@gmail.com>
2023-12-03 16:42:32 +0600

    Updated

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -5,11 +5,26 @@ const getLastStudentId = async () => {
   const lastStudent = await User.findOne({ role: "student" }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester | null) => {
-  const currentId = (await getLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await getLastStudentId();
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentSemesterYear = payload?.year;
+  const currentSemesterCode = payload?.code;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterYear === currentSemesterYear &&
+    lastStudentSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementedId: string = (Number(currentId) + 1)
     .toString()
     .padStart(4, "0");

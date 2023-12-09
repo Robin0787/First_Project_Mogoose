52b52f53a4bba8494a70cc2c058e61a55e8f7e2f
Robin <mohammadrobin636@gmail.com>
2023-12-09 16:17:04 +0600

    'Updated'

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,31 +1,79 @@
 import httpStatus from "http-status";
 import mongoose from "mongoose";
+import QueryBuilder from "../../builder/QueryBuilder";
 import { AppError } from "../../errors/AppError";
 import { User } from "../user/user.model";
+import { studentSearchableFields } from "./student.constant";
 import { TStudent } from "./student.interface";
 import { Student } from "./student.model";
 
+// const getAllStudents = async (query: Record<string, unknown>) => {
+//   const studentSearchableFields = ["email", "name.firstName", "presentAddress"];
+
+//   let searchTerm = "";
+//   if (query?.searchTerm) {
+//     searchTerm = query?.searchTerm as string;
+//   }
+
+//   const searchQuery = Student.find({
+//     $or: studentSearchableFields.map((field) => ({
+//       [field]: { $regex: searchTerm, $options: "i" },
+//     })),
+//   });
+
+//   const copyQueryObj = { ...query };
+
+//   const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
+//   excludeFields.forEach((el) => delete copyQueryObj[el]);
+
+//   const filterQuery = searchQuery
+//     .find(copyQueryObj)
+//     .populate("admissionSemester")
+//     .populate({
+//       path: "academicDepartment",
+//       populate: {
+//         path: "academicFaculty",
+//       },
+//     });
+
+//   let sort = "-createdAt";
+//   if (query.sort) {
+//     sort = query?.sort as string;
+//   }
+//   const sortQuery = filterQuery.sort(sort);
+
+//   let page = 1;
+//   let limit = 1;
+//   let skip = 0;
+
+//   if (query.limit) {
+//     limit = Number(query?.limit);
+//   }
+
+//   if (query.page) {
+//     page = Number(query?.page);
+//     skip = (page - 1) * limit;
+//   }
+
+//   const skipQuery = sortQuery.skip(skip);
+
+//   const limitQuery = skipQuery.limit(limit);
+
+//   // field limiting
+
+//   let fields = "-__v";
+
+//   if (query.fields) {
+//     fields = (query?.fields as string).split(",").join(" ");
+//   }
+
+//   const fieldsQuery = await limitQuery.select(fields);
+
+//   return fieldsQuery;
+// };
+
 const getAllStudents = async (query: Record<string, unknown>) => {
-  const studentSearchableFields = ["email", "name.firstName", "presentAddress"];
-
-  let searchTerm = "";
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string;
-  }
-
-  const searchQuery = Student.find({
-    $or: studentSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: "i" },
-    })),
-  });
-
-  const copyQueryObj = { ...query };
-
-  const excludeFields = ["searchTerm", "sort", "limit", "page", "fields"];
-  excludeFields.forEach((el) => delete copyQueryObj[el]);
-
-  const filterQuery = searchQuery
-    .find(copyQueryObj)
+  const modelQueryForBuilder = Student.find()
     .populate("admissionSemester")
     .populate({
       path: "academicDepartment",
@@ -34,40 +82,14 @@ const getAllStudents = async (query: Record<string, unknown>) => {
       },
     });
 
-  let sort = "-createdAt";
-  if (query.sort) {
-    sort = query?.sort as string;
-  }
-  const sortQuery = filterQuery.sort(sort);
-
-  let page = 1;
-  let limit = 1;
-  let skip = 0;
-
-  if (query.limit) {
-    limit = Number(query?.limit);
-  }
-
-  if (query.page) {
-    page = Number(query?.page);
-    skip = (page - 1) * limit;
-  }
-
-  const paginatedQuery = sortQuery.skip(skip);
-
-  const limitQuery = paginatedQuery.limit(limit);
-
-  // field limiting
-
-  let fields = "-_v";
-
-  if (query.fields) {
-    fields = (query?.fields as string).split(",").join(" ");
-  }
-
-  const fieldsQuery = await limitQuery.select(fields);
-
-  return fieldsQuery;
+  const studentQuery = new QueryBuilder(modelQueryForBuilder, query)
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .filterFields();
+  const result = await studentQuery.modelQuery;
+  return result;
 };
 
 const getSingleStudent = async (id: string) => {

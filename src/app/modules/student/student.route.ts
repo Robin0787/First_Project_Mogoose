df2ee9704b4dd398de1556b88e49fdd699446bb3
Robin <mohammadrobin636@gmail.com>
2024-01-03 15:26:09 +0600

    'Updated'

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
+import { USER_ROLE } from "../user/user.constant";
 import { studentControllers } from "./student.controller";
 import { studentValidations } from "./student.validation";
 
 const router = express.Router();
 
 router.get("/", studentControllers.getAllStudents);
-router.get("/:id", studentControllers.getSingleStudent);
+router.get(
+  "/:id",
+  auth(USER_ROLE.faculty, USER_ROLE.admin),
+  studentControllers.getSingleStudent,
+);
 
 router.patch(
   "/:id",

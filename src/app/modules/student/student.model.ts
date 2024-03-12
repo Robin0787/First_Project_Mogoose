df7022c8c2baa536ecbc6533484253c749bacbb3
Robin <mohammadrobin636@gmail.com>
2024-03-12 12:52:56 +0600

    Updated

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -131,7 +131,7 @@ const studentSchema = new Schema<TStudent, StudentModel>(
 
 // virtual
 studentSchema.virtual("fullName").get(function () {
-  return `${this?.name?.firstName}  ${this?.name?.middleName} ${this?.name?.lastName}`;
+  return `${this?.name?.firstName} ${this?.name?.lastName}`;
 });
 
 // Query Middleware
